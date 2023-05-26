6e6a7d0216daab12a3daa23747e56242865dfda3
CraciDenisStefan <crdenis13@gmail.com>
2023-05-26 17:27:58 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -5,17 +5,27 @@ let start = document.querySelector(`.start_100`);
 function createSquareElement(){
     let square = document.createElement(`div`);
     square.classList.add(`square`);
+    
     return square;
 }
 
 
 start.addEventListener(`click`, function(){
 
-    let container = document.querySelector(`.container`)
+    let container = document.querySelector(`.container_main`)
+    container.classList.add(`container`)
 
     for (let i=0; i<100; i++){
         let square = createSquareElement();
+        square.innerText = i + 1;
+       
         container.append(square)
+
+    square.addEventListener(`click`, function(){
+        this.classList.add(`clicked`)
+        console.log(this);
+    })
+
     }
 
 
