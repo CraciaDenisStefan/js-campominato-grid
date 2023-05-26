5f8ed3d6023fc599f6cc8a51bca0966cc534e0c9
CraciDenisStefan <crdenis13@gmail.com>
2023-05-26 16:33:44 +0200

    [FEAT]Griglia creata

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+let start = document.querySelector(`.start_100`);
+
+function createSquareElement(){
+    let square = document.createElement(`div`);
+    square.classList.add(`square`);
+    return square;
+}
+
+
+start.addEventListener(`click`, function(){
+
+    let container = document.querySelector(`.container`)
+
+    for (let i=0; i<100; i++){
+        let square = createSquareElement();
+        container.append(square)
+    }
+
+
+})
